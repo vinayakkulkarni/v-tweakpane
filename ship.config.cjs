fd6423c341e579e4c8c183ff94bf4f88e3af6b1d
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2024-03-20 10:59:26 +0530

    fix: let shipjs do publishCommand
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- ship.config.cjs
+++ ship.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   installCommand: () => 'bun i',
-  publishCommand: () => 'bun publish --tag latest',
   beforeCommitChanges: ({ exec }) => {
     exec('./scripts/bump-jsr-version.cjs');
   },
