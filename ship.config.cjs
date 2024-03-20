bf83883fabbc532baa06ed3892bb16da593cbe5b
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2024-03-20 10:28:36 +0530

    fix: bump jsr package
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- ship.config.cjs
+++ ship.config.cjs
@@ -1,15 +1,7 @@
 module.exports = {
   installCommand: () => 'bun i',
   publishCommand: () => 'bun publish --tag latest',
-  beforeCommitChanges: ({ nextVersion, dir }) => {
-    console.log('next version :::', nextVersion);
-    console.log('jsr:::', dir('jsr.json').read());
-    // update jsr.json version key to be nextVersion
-    dir('jsr.json').update((content) => {
-      console.log('jsr.json content :::', content);
-      const json = JSON.parse(content);
-      json.version = nextVersion;
-      return JSON.stringify(json, null, 2);
-    });
+  beforeCommitChanges: ({ exec }) => {
+    exec('./scripts/bump-jsr-version.cjs');
   },
 };
